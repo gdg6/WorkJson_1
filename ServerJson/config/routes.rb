bd81b43e9eac7293db340b32572b683fb0b0dcc7
gdg6 <zhulvern-92@mail.ru>
2015-05-15 07:18:35 +0000

    Правки продакшена

--- ServerJson/config/routes.rb
+++ ServerJson/config/routes.rb
@@ -25,8 +25,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new', as: :login
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy', as: :logout
-
-
+ 
   get 'registration' => 'users#new', as: :registration
   post 'registration' => 'users#create'
 
